daa639591b7da5c445947ca4add754003f0d8b22
Hai Hoang nguyen <hoangngn.26@gmail.com>
2025-08-24 22:25:23 +0700

    host dimension

---
+++ models/core/dim_host.sql
@@ -0,0 +1,25 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key='host_id'
+    ) 
+}}
+
+with dim_hosts as (
+    select
+        "id" as host_id,
+        "name" as host_name,
+        "is_superhost" as is_superhost,
+        "created_at" as created_at,
+        "updated_at" as updated_at
+    from {{ source('airbnb', 'hosts') }}
+)
+
+select * 
+from dim_hosts
+
+{% if is_incremental() %}
+  -- For incremental loads: only bring in rows updated since last run
+  where updated_at > (select max(updated_at) from {{ this }})
+{% endif %}
+    
